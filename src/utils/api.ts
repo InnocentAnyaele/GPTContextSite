670f60d3f1d3053baada1df053b37bb040bcab70
InnocentAnyaele <luisnerma545@gmail.com>
2023-05-14 08:37:37 +0000

    commented out unsused apis

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,12 @@
 
-const localAPI:string = 'http://127.0.0.1:5000'
+//const localAPI:string = 'http://127.0.0.1:5000'
 
-const renderAPI:string = 'https://gptcontext.onrender.com'
+//const renderAPI:string = 'https://gptcontext.onrender.com'
 
 // doesn't support excel
-const pythonAnywhereAPI:string = 'https://1nnocent.pythonanywhere.com'
+//const pythonAnywhereAPI:string = 'https://1nnocent.pythonanywhere.com'
 
+// only supports PDF
 const renderAPI2:string = "https://gptcontextserver2.onrender.com"
 
 export const api:string = renderAPI2
